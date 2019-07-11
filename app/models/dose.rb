59932f206865f62354ccbef285fd5631b60273a3
Arne Pedersen <arne.lamo@gmail.com>
2019-07-11 12:24:49 +0200

    models OK

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates :ingredient, uniqueness: { scope: :cocktail }
+  # validates :ingredient, uniqueness: { scope: :cocktail }
+  validates_uniqueness_of :ingredient_id, scope: :cocktail_id
 end
